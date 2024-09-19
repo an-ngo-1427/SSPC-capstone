e64c4a81a6e23ecfb6607638e8be867d9af42861
Gia Ng <giang@tgtn.local>
2024-09-19 15:21:51 -0700

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,44 @@
+import React from "react";
+import "./App.css";
+
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <nav className="nav">
+          <h1 className="logo">Pepper's Happy Trails</h1>
+          <ul className="nav-links">
+            <li>
+              <a href="#appointments">Reserve your walk!</a>
+            </li>
+            <li>
+              <a href="#about">Who me?</a>
+            </li>
+            <li>
+              <a href="#contact"></a>Contact
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="hero">
+        <h2>Walk with the Earth</h2>
+        <p>Your dog's best friend in an eco-conscious world. </p>
+        <button className="cta-button">Reserve your Walk!</button>
+      </section>
+
+      <footer className="footer">
+        <p>&copy; 2024 Pepper's Happy Trails. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +74,4 @@ function App() {
 }
 
 export default App
+*/
